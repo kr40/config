aaf2fdddca5898eb279fc7a77cbced7ea1cb81bb
kr40 <kr40.git@gmail.com>
2024-01-17 13:27:00 +0530

    Changed install script

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,28 +4,30 @@
 # dotfiles folder so that the links are set properly!
 
 function bannerSimple() {
-	local msg="${2} ${1} ${2}"
-	local edge
-	edge=${msg//?/$2}
-	echo "${edge}"
-	echo "$(tput bold)${msg}$(tput sgr0)"
-	echo "${edge}"
-	echo
+  local msg="${2} ${1} ${2}"
+  local edge
+  edge=${msg//?/$2}
+  echo "${edge}"
+  echo "$(tput bold)${msg}$(tput sgr0)"
+  echo "${edge}"
+  echo
 }
 
 bannerSimple "kr40's Bash DotFiles Installer" "*"
 
-echo "Moving already existing .bashrc and .bash_aliases to bashrc.bak and bashaliaes.bak"
+echo "Moving already existing .bashrc, .bash_aliases and .gitconfig to bashrc.bak, bashaliaes.bak and gitconfig.bak"
 
 # Rename the existing bashrc and bash_aliases files.
-mv "$HOME/.bashrc" "$HOME/bashrc.bak"
-mv "$HOME/.bash_aliases" "$HOME/bash_aliases.bak"
+mv "$HOME/.bashrc" "$HOME/bashrc.bak" >>/dev/null 2>&1
+mv "$HOME/.bash_aliases" "$HOME/bash_aliases.bak" >>/dev/null 2>&1
+mv "$HOME/.gitconfig" "$HOME/gitconfig.bak" >>/dev/null 2>&1
 
-echo "Symlinking alises.sh and bashrc.sh to .bashrc and .bash_aliases"
+echo "Symlinking alises.sh, bashrc.sh and gitconfig to .bashrc, .bash_aliases and .gitconfig
 
 # Create links for the shell configuration.
 ln -sf "$PWD/aliases.sh" "$HOME/.bash_aliases"
 ln -sf "$PWD/bashrc.sh" "$HOME/.bashrc"
+ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
 
 echo "Successfully installed dotfiles!"
 echo "Read system-setup for further information."
